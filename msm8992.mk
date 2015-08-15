33013a18f3df9edefef7fd7befc11fc05e831e25
Manoj Kumar AVM <manojavm@codeaurora.org>
2015-08-14 19:16:39 -0700

    msm8992:  WfdCommon to PRODUCT_BOOT_JARS
    Add WfdCommon to PRODUCT_BOOT_JARS to enable Miracast
    feature through display settings app
    
    Change-Id: I540084d8b6afc584581928767f875751abc805d3

--- msm8992.mk
+++ msm8992.mk
@@ -45,7 +45,7 @@ ifneq ($(strip $(QCPATH)),)
 PRODUCT_BOOT_JARS += qcom.fmradio
 PRODUCT_BOOT_JARS += com.qti.dpmframework
 PRODUCT_BOOT_JARS += oem-services
-#PRODUCT_BOOT_JARS += WfdCommon
+PRODUCT_BOOT_JARS += WfdCommon
 #PRODUCT_BOOT_JARS += extendedmediaextractor
 #PRODUCT_BOOT_JARS += security-bridge
 #PRODUCT_BOOT_JARS += qsb-port
