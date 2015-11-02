7b36621b5a1b8217fa8cf58371be575f0f300919
Kiran Raparthy <krapar@codeaurora.org>
2015-11-02 19:49:22 +0530

    msm8992: add temperature related sensors to PRODUCT_COPY_FILES
    msm8992 supports ambient temperature and relative humidity sensor.
    
    Change-Id: I536a1537ce0d4655cd7c4764898e77757579526b

--- msm8992.mk
+++ msm8992.mk
@@ -113,6 +113,8 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.barometer.xml:system/etc/permissions/android.hardware.sensor.barometer.xml \
     frameworks/native/data/etc/android.hardware.sensor.stepcounter.xml:system/etc/permissions/android.hardware.sensor.stepcounter.xml \
     frameworks/native/data/etc/android.hardware.sensor.stepdetector.xml:system/etc/permissions/android.hardware.sensor.stepdetector.xml \
+    frameworks/native/data/etc/android.hardware.sensor.ambient_temperature.xml:system/etc/permissions/android.hardware.sensor.ambient_temperature.xml \
+    frameworks/native/data/etc/android.hardware.sensor.relative_humidity.xml:system/etc/permissions/android.hardware.sensor.relative_humidity.xml \
 
 PRODUCT_COPY_FILES += \
     device/qcom/msm8992/sensors/hals.conf:system/etc/sensors/hals.conf
