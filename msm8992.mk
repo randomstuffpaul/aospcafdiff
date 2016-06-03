13ca75c0151b9fab3804d8ff38e0d444ffd3bca4
Prasath Balakrishnan <bpras@codeaurora.org>
2016-06-03 18:31:33 +0530

    Telephony(MSIM): Add telephony-ext to boot jar path and PRODUCT_PACKAGES
    Include telephony-ext jar library in PRODUCT_PACKAGES
    and in boot jar path.
    telephony-ext jar is needed for MSIM manual provisioning
    and for other telephony related functionalities to work
    
    Change-Id: I049ec39e88ec734e3770a9439d4c7ba8949b4357

--- msm8992.mk
+++ msm8992.mk
@@ -43,6 +43,11 @@ PRODUCT_MODEL := MSM8992 for arm64
 
 #PRODUCT_BOOT_JARS += vcard
 PRODUCT_BOOT_JARS += tcmiface
+# This jar is needed for MSIM manual provisioning and for other
+# telephony related functionalities to work.
+PRODUCT_BOOT_JARS += telephony-ext
+
+PRODUCT_PACKAGES += telephony-ext
 #PRODUCT_BOOT_JARS += qcmediaplayer
 #PRODUCT_BOOT_JARS += org.codeaurora.Performance
 
